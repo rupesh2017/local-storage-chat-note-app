206830f89ec3848be7156c7b046b318fb99f5f94
rupesh2017 <rupesh.sak94@gmail.com>
2018-09-18 15:40:10 +0530

    user and channel list

--- room_private/static/private.js
+++ room_private/static/private.js
@@ -3,34 +3,20 @@ document.addEventListener('DOMContentLoaded',()=>{
    
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-
+    const senduser =localStorage.getItem('usersid');
+    
     socket.on('connect',()=>{
-        document.querySelector('#submit-user').onclick= ()=>{
-         const user = document.querySelector('#user').value;
-         
-          socket.emit('submit user',{'user':user});      
-          return false;
-        
-        };
-        
-    
-    
+
             document.querySelector('#new-message').onclick=function(){
             
                 const selection = document.querySelector('#message').value;
+                console.log(senduser);
                 socket.emit('submit message',{'selection':selection,'senduser':senduser});
                 return false;
             };
                
     });
 
-    socket.on('announce user',(data)=>{
-        const option = document.createElement('option');        
-       option.innerHTML=`${data.user}`;
-       
-        document.querySelector('#add-user').append(option);
-
-    });
 
     socket.on('announce message',(data)=>{
         const li = document.createElement('li');
